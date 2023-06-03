52da4067de66bf3c66589921beebabf7431cb664
nbhati <naresh.bhati@finicity.com>
2023-06-03 20:01:24 +0530

    kube-fisrt-app

---
+++ kub-action-01-starting-setup/app.js
@@ -0,0 +1,18 @@
+/** @format */
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>Hello from this NodeJS app!!!!Updated!!! 4th</h1>
+    <p>Try sending a request to /error and see what happens</p>
+  `);
+});
+
+app.get("/error", (req, res) => {
+  process.exit(1);
+});
+
+app.listen(8080);
