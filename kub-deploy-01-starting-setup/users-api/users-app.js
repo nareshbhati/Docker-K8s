181c80610d38a475f5499352113299670b8003f7
nbhati <naresh.bhati@finicity.com>
2023-07-22 21:08:48 +0530

    chore: added file

---
+++ kub-deploy-01-starting-setup/users-api/users-app.js
@@ -0,0 +1,47 @@
+/** @format */
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const userRoutes = require("./routes/user-routes");
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.use(userRoutes);
+
+app.use((err, req, res, next) => {
+  let code = 500;
+  let message = "Something went wrong.";
+  if (err.code) {
+    code = err.code;
+  }
+
+  if (err.message) {
+    message = err.message;
+  }
+  res.status(code).json({ message: message });
+});
+
+console.log("trying to connecting mongo db");
+mongoose.connect(
+  process.env.MONGODB_CONNECTION_URI,
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) {
+      console.log("COULD NOT CONNECT TO MONGODB!");
+    } else {
+      console.log("connected Mongo");
+      app.listen(3000);
+    }
+  }
+);
